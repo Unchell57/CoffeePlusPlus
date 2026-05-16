4fb4b568ac22465570f8984fb8729fb6cc982ecd
Unchell57 <VolozheninVadim@gmail.com>
2026-05-16 15:22:21 +0300

    Parsing UPD Started to parse several goods

--- code/w_parsing.py
+++ code/w_parsing.py
@@ -1,7 +1,20 @@
 import asyncio
 from w_parsers import MetroParser
 
-# парсим предложение молока
-milk: MetroParser = MetroParser("molochnye-prodkuty-syry-i-yayca/moloko")
-asyncio.run( milk.parse() )
-milk.to_csv("milk")
+# создаём парсеры для всех интересных нам товаров, запускаем их и сохраняем полученные данные
+
+parsers_data: dict[str, str] = {
+    "milk": "molochnye-prodkuty-syry-i-yayca/moloko",
+    "zoomer_milk": "molochnye-prodkuty-syry-i-yayca/rastitelnye-produkty/rastitelnoe-moloko",
+    "sport_food": "zdorovoe-pitanie/sportivnoe-pitanie",
+    "salads": "gotovye-bljuda-polufabrikaty/salaty",
+    "drinks": "bezalkogolnye-napitki/soki-morsy-nektary",
+}
+
+async def main():
+    parsers = [MetroParser(path) for path in parsers_data.values()]
+    await asyncio.gather(*(parser.parse() for parser in parsers))
+    for parser, (good, _) in zip(parsers, parsers_data.items()):
+        parser.to_csv(good)
+
+asyncio.run(main())
